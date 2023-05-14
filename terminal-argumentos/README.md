d47af53f08514e7dd9e1deca4f3ca33c214d6e4b
Alym62 <gasafm62@gmail.com>
2023-05-14 16:24:27 -0300

    Começando com Objetos.

---
+++ terminal-argumentos/README.md
@@ -0,0 +1,3 @@
+## Bem-vindo aos meus primeiros códigos em Java.☕´
+
+- Espero que gostem desse repositório que todo código que eu desenvolver, eu vou postar aqui.😊
---
+++ terminal-argumentos/src/MinhaClasse.java
@@ -0,0 +1,5 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Oi Tropa, fui executado pelo terminal!");
+    }
+}
